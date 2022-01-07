16090d3c2b27be95a8f30a681bdb1c01f135ffa2
Brigitte <brigitte.giguere.x@gmail.com>
2022-01-07 17:23:06 -0500

    Added GitHub and Netlify links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/brigittex/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Brigitte Giguère 🍁
+      </footer>
+    </div>
+  );
 }
