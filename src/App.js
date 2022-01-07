02d7b89c0cc2a4424360f023d883a94014a6d1c8
Brigitte <brigitte.giguere.x@gmail.com>
2022-01-07 17:34:33 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/brigittex/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Brigitte Giguère 🍁
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/brigittex/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Brigitte Giguère 🍁
+        </footer>
+      </div>
     </div>
   );
 }
