c85ed27eb0a1327a27adcada5ace7c1d6468557b
Brigitte <brigitte.giguere.x@gmail.com>
2022-01-08 16:59:00 -0500

    Forecast CSS

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,20 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon.js";
+import "./Forecast.css";
 
 export default function Forecast() {
   return (
     <div className="Forecast">
       <div className="row">
-        <div className="col-2">
-          <div>Thu</div>
-          <WeatherIcon icon={"01d"} size={65} />
+        <div className="col">
+          <div className="Forecast-day">Thu</div>
+          <WeatherIcon icon={"01d"} size={50} />
           <div>
-            <span>19</span>
-            <span>10</span>
+            <span className="Forecast-min">-19°</span>
+            <span className="Forecast-max">-10°</span>
           </div>
         </div>
       </div>
     </div>
   );
 }
-//<WeatherIcon weather={(icon = "01d")} />
