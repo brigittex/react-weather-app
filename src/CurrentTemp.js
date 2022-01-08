aaa151704266b019f5492b21dbc94aa23c87c6c4
Brigitte <brigitte.giguere.x@gmail.com>
2022-01-07 22:47:11 -0500

    Added unit conversion to CurrentTemp component

--- src/CurrentTemp.js
+++ src/CurrentTemp.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CurrentTemp.css";
 
 export default function CurrentTemp(props) {
-  return (
-    <div className="CurrentTemp">
-      <span>{props.weather.temperature}</span>
-      <small>°C</small>
-    </div>
-  );
+  let celsius = props.weather.temperature;
+  let fahrenheit = Math.round(celsius * (9 / 5) + 32);
+  let [unit, setUnit] = useState("C");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("F");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("C");
+  }
+
+  if (unit === "C") {
+    return (
+      <div className="CurrentTemp">
+        <span>{celsius}</span>
+        <small>
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </small>
+      </div>
+    );
+  } else {
+    return (
+      <div className="CurrentTemp">
+        <span>{fahrenheit}</span>
+        <small>
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </small>
+      </div>
+    );
+  }
 }
