38d5ba82fe2104a8b797386ca6b4283a86042e33
Brigitte <brigitte.giguere.x@gmail.com>
2022-01-08 18:34:49 -0500

    Added functions within ForecastDay

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -2,17 +2,30 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon.js";
 
 export default function ForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay;
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
+  function minTemp() {
+    let min = Math.round(props.data[0].temp.min);
+    return `${min}°`;
+  }
+
+  function maxTemp() {
+    let max = Math.round(props.data[0].temp.max);
+    return `${max}°`;
+  }
+
   return (
     <div>
-      <div className="Forecast-day">Thu</div>
+      <div className="Forecast-day">{day()}</div>
       <WeatherIcon icon={props.data[0].weather[0].icon} size={50} />
       <div>
-        <span className="Forecast-min">
-          {Math.round(props.data[0].temp.min)}°
-        </span>
-        <span className="Forecast-max">
-          {Math.round(props.data[0].temp.max)}°
-        </span>
+        <span className="Forecast-min">{minTemp()}</span>
+        <span className="Forecast-max">{maxTemp()}</span>
       </div>
     </div>
   );
